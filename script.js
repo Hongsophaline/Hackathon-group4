3f4fa4edc29f8abd42269287fa8c3bde84d3c85f
Hongsophaline <hong.sophaline@institute.pse.ngo>
2025-01-24 09:22:39 +0700

    update calculate

---
+++ script.js
@@ -0,0 +1,21 @@
+// calculator program
+
+const display = document.getElementById("display");
+
+function appendtoDisplay(input) {
+  display.value += input;
+}
+
+function clearDisplay() {
+  display.value = "";
+}
+
+function calculate() {
+  display.value = eval(display.value);
+}
+function conversion(input) {
+  display.value /= input;
+}
+function currency(input) {
+  display.value = input + display.value;
+}
